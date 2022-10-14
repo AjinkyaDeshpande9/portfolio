5ac6d0c893c0cac28713afcd22a85773be17ec48
Ajinkya <deshpande.ajinkya09@gmail.com>
2022-10-14 21:49:30 +0530

    Initial setup and Header addded

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Ajinkya's Portfolio</title>
+      </Head>
+      {/* Header */}
+      <Header/>
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+
+    </div>
+  )
+}
+
+export default Home
