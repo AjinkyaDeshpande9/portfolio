0b08425a470e071dff15eca81101b03083e75025
Ajinkya Deshpande <deshpande.ajinkya09@gmail.com>
2023-01-03 01:15:16 +0530

    Hero Section Completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import About from '../components/About'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -19,6 +20,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* About */}
+      <section id='about' className='snap-center'>
+        <About/>
+      </section>
 
       {/* Experience */}
 
